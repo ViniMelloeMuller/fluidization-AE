2dc44743ac49b35be2b486c95336133407ac7304
Vinicius Mello e Muller <viniciusmelloemuller@gmail.com>
2024-10-09 11:13:33 -0300

    Refeita a coleta de dados, mudando a posição do sensor PT105

---
+++ data/CALIBRACAO/curvas.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import os 
+import matplotlib.pyplot as plt
+
+FTMEDIO = []
+PTMEDIO = []
+for filename in os.listdir():
+    if filename.endswith(".csv"):
+        df = pd.read_csv(filename, delimiter=",")
+        FTMEDIO.append(df.loc[:, "cDAQ1Mod1/ai3"].mean())
+        PTMEDIO.append(df.loc[:, "cDAQ1Mod1/ai2"].mean())
+
+dataframe = {
+        "FTMEDIO":FTMEDIO,
+        "PTMEDIO":PTMEDIO
+        }
+df = pd.DataFrame(dataframe)
+df.to_excel("MEDIAS.xlsx")
